b9c37da41298004226cf32a62874413c4e57a69d
huixing <wanghuixing24@gmail.com>
2021-10-19 15:43:08 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.21"
     id("org.jetbrains.compose") version "1.0.0-alpha2"
+    id("maven-publish")
 }
 
-group = "me.lierxiao"
+group = "com.github.HuixingWong"
 version = "1.0"
 
 repositories {
@@ -34,12 +35,25 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.HuixingWong"
+                artifactId = "constraint"
+                version = "1.0"
+            }
+        }
+    }
+}
+
+
 compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "untitled"
+            packageName = "constraintlayout"
             packageVersion = "1.0.0"
         }
     }
